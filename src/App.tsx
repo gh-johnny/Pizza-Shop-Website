ffc8a05a2dcba740eae49b3870649b53d776a528
gh-johnny <jmfurtadoromero@gmail.com>
2024-03-11 19:00:36 -0300

    chore: eslint + prettier configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import { Button } from './components/ui/button'
 import './global.css'
 
+import { Button } from './components/ui/button'
+
 export function App() {
-
-    return (
-        <>
-            <div className='flex justify-center items-center h-screen'>
-                <Button>Send</Button>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className='flex justify-center items-center h-screen'>
+        <Button>Send</Button>
+      </div>
+    </>
+  )
 }
-

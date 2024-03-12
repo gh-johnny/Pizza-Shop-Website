22d5cd4637f118661299e0b521cabc261ac0992b
gh-johnny <jmfurtadoromero@gmail.com>
2024-03-12 15:57:50 -0300

    chore: Helmet (lib) added

--- src/pages/app/dashboard.tsx
+++ src/pages/app/dashboard.tsx
@@ -1,3 +1,11 @@
+import { Helmet } from 'react-helmet-async'
+
 export function Dashboard() {
-  return <h1>Dashboard</h1>
+  return (
+    <>
+      <Helmet title='Dashboard' />
+      <h1>Dashboard</h1>
+    </>
+  )
 }
+

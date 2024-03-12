7b56a56b5c6b9017accbcd96e788e6b24e467820
gh-johnny <jmfurtadoromero@gmail.com>
2024-03-12 16:44:28 -0300

    feat: login shadcn-ui (blue) theme

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,10 +1,36 @@
 import { Helmet } from 'react-helmet-async'
 
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
 export function SignIn() {
   return (
     <>
       <Helmet title='Login' />
-      <h1>SignIn</h1>
+      <div className='p-8'>
+        <div className='w-[350px] flex flex-col justify-center gap-6'>
+          <section className='flex flex-col gap-2 text-center'>
+            <h1 className='text-2xl font-semibold tracking-tight'>
+              Access dashboard
+            </h1>
+            <p className='text-sm text-muted-foreground'>
+              Keep up with your sales through the partner dashboard!
+            </p>
+          </section>
+
+          <form className='space-y-4'>
+            <div className='space-y-2'>
+              <Label>Your e-mail</Label>
+              <Input id='email' type='email' />
+            </div>
+
+            <Button className='w-full' type='submit'>
+              Access Dashboard
+            </Button>
+          </form>
+        </div>
+      </div>
     </>
   )
 }

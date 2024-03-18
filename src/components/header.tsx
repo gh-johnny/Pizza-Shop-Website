9b229f8d9786762bbc1958bca82f5afae69db526
gh-johnny <jmfurtadoromero@gmail.com>
2024-03-18 15:44:02 -0300

    feat: header for main app

---
+++ src/components/header.tsx
@@ -0,0 +1,27 @@
+import { Home, Pizza, UtensilsCrossed } from 'lucide-react'
+
+import { NavLink } from './nav-link'
+import { Separator } from './ui/separator'
+
+export default function Header() {
+  return (
+    <div className='border-b'>
+      <div className='flex h-16 items-center gap-6 px-6'>
+        <Pizza className='h-6 w-6' />
+
+        <Separator orientation='vertical' className='h-6' />
+
+        <nav className='flex items-center space-x-4 lg:space-x-6'>
+          <NavLink to='/'>
+            <Home className='h-4 w-4' />
+            Dashboard
+          </NavLink>
+          <NavLink to='/'>
+            <UtensilsCrossed className='h-4 w-4' />
+            Requests
+          </NavLink>
+        </nav>
+      </div>
+    </div>
+  )
+}

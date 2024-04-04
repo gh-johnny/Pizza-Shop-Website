b8a08fe94516d1f83019ac2b521bc275c5f6e0e1
gh-johnny <jmfurtadoromero@gmail.com>
2024-04-04 14:35:39 -0300

    chore: try catch block for localstorage theme

--- src/components/theme/theme-provider.tsx
+++ src/components/theme/theme-provider.tsx
@@ -26,9 +26,14 @@ export function ThemeProvider({
   storageKey = 'vite-ui-theme',
   ...props
 }: ThemeProviderProps) {
-  const [theme, setTheme] = useState<Theme>(
-    () => (localStorage.getItem(storageKey) as Theme) || defaultTheme,
-  )
+  const [theme, setTheme] = useState<Theme>(() => {
+    try {
+      return (localStorage.getItem(storageKey) as Theme) || defaultTheme
+    } catch (err) {
+      console.error(err)
+      return defaultTheme
+    }
+  })
 
   useEffect(() => {
     const root = window.document.documentElement

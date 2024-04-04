cdbebf64d0ed3e2487c82ecf5559299841a66164
gh-johnny <jmfurtadoromero@gmail.com>
2024-04-04 12:52:04 -0300

    feat: account menu for header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,6 @@
 import { Home, Pizza, UtensilsCrossed } from 'lucide-react'
 
+import AccountMenu from './account-menu'
 import { NavLink } from './nav-link'
 import { ThemeToggle } from './theme/theme-toggle'
 import { Separator } from './ui/separator'
@@ -25,6 +26,7 @@ export default function Header() {
 
         <div className='ml-auto flex items-center gap-2'>
           <ThemeToggle />
+          <AccountMenu />
         </div>
       </div>
     </div>

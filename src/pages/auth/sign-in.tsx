949b33682d6a93487c76d0ac16d1cc1ff54e9141
gh-johnny <jmfurtadoromero@gmail.com>
2024-04-05 21:15:30 -0300

    chore: sign-up api req

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,7 +1,7 @@
 import { useMutation } from '@tanstack/react-query'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
-import { Link } from 'react-router-dom'
+import { Link, useSearchParams } from 'react-router-dom'
 import { toast } from 'sonner'
 
 import { singIn } from '@/api/sign-in'
@@ -11,19 +11,25 @@ import { Label } from '@/components/ui/label'
 import { TSignInSchema } from '@/schemas/signInSchema'
 
 export function SignIn() {
+  const [searchParams] = useSearchParams()
+
   const {
     register,
     handleSubmit,
     formState: { isSubmitting },
-  } = useForm<TSignInSchema>()
+  } = useForm<TSignInSchema>({
+    defaultValues: {
+      email: searchParams.get('email') ?? '',
+    },
+  })
 
   const { mutateAsync: authenticate } = useMutation({
     mutationFn: singIn,
   })
 
   async function handleSignIn(data: TSignInSchema) {
-    await authenticate({ email: data.email })
     try {
+      await authenticate({ email: data.email })
       toast.success('A link was sent to your e-mail.', {
         action: {
           label: 'Resend',
--- src/pages/auth/sign-up.tsx
+++ src/pages/auth/sign-up.tsx
@@ -1,8 +1,10 @@
+import { useMutation } from '@tanstack/react-query'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
 import { Link, useNavigate } from 'react-router-dom'
 import { toast } from 'sonner'
 
+import { registerRestaurant } from '@/api/register-restaurant'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
@@ -17,14 +19,22 @@ export function SignUp() {
     formState: { isSubmitting },
   } = useForm<TSignUpSchema>()
 
+  const { mutateAsync: registerRestaurantReq } = useMutation({
+    mutationFn: registerRestaurant,
+  })
+
   async function handleSignIn(data: TSignUpSchema) {
-    await new Promise((resolve) => setTimeout(resolve, 2000))
     try {
-      console.log(data)
+      await registerRestaurantReq({
+        restaurantName: data.restaurantName,
+        managerName: data.managerName,
+        email: data.email,
+        phone: data.phone,
+      })
       toast.success('Restaurant account successfully created', {
         action: {
           label: 'Login',
-          onClick: () => navigate('/sign-in'),
+          onClick: () => navigate(`/sign-in?email=${data.email}`),
         },
       })
     } catch (err) {
